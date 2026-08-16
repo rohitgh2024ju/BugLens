3066605ef9af5a28659d5ed8ea155626b2ccad38
rohitgh2024ju <rohitstudying2004@gmail.com>
2026-08-16 17:04:06 +0530

    Initial commit

---
+++ buglens/src/main/java/dev/rohit/buglens/BuglensApplication.java
@@ -0,0 +1,13 @@
+package dev.rohit.buglens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuglensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuglensApplication.class, args);
+	}
+
+}
---
+++ buglens/src/test/java/dev/rohit/buglens/BuglensApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rohit.buglens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuglensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
