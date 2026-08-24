2a2d3bdc8061bd5e02c7d25454694a1618d3e9ce
rohitgh2024ju <rohitstudying2004@gmail.com>
2026-08-24 22:43:18 +0530

    create config file and impl load mapping

--- buglens/src/main/java/dev/rohit/buglens/NormalizerEngine/MappingLoader.java
+++ buglens/src/main/java/dev/rohit/buglens/NormalizerEngine/MappingLoader.java
@@ -1,5 +1,118 @@
 package dev.rohit.buglens.NormalizerEngine;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public class MappingLoader {
 
+    public void load() {
+        try {
+            XmlMapper xmlMapper = new XmlMapper();
+
+            // Deserialize using the root wrapper class
+            NormalizationMappings mappings = xmlMapper.readValue(
+                    new File("buglens/config/normalization-mapping.xml"),
+                    NormalizationMappings.class);
+
+            if (mappings.getFormats() != null) {
+                for (FormatConfig format : mappings.getFormats()) {
+                    System.out.println("=== Format ID: " + format.getId() + " ===");
+
+                    if (format.getFields() != null) {
+                        for (FieldMapping field : format.getFields()) {
+                            System.out.println("  Source: " + field.getSource() + " -> Target: " + field.getTarget());
+                        }
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println("Failed to parse normalization-mapping.xml: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MappingLoader mappingLoader = new MappingLoader();
+        mappingLoader.load();
+    }
+}
+
+// Top-level container matching <NormalizationMappings>
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JacksonXmlRootElement(localName = "NormalizationMappings")
+class NormalizationMappings {
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "Format")
+    private List<FormatConfig> formats;
+
+    public List<FormatConfig> getFormats() {
+        return formats;
+    }
+
+    public void setFormats(List<FormatConfig> formats) {
+        this.formats = formats;
+    }
+}
+
+// Container matching each <Format id="...">
+@JsonIgnoreProperties(ignoreUnknown = true)
+class FormatConfig {
+
+    @JacksonXmlProperty(isAttribute = true)
+    private String id;
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "Field")
+    private List<FieldMapping> fields;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<FieldMapping> getFields() {
+        return fields;
+    }
+
+    public void setFields(List<FieldMapping> fields) {
+        this.fields = fields;
+    }
+}
+
+// Container matching each <Field source="..." target="..."/>
+class FieldMapping {
+
+    @JacksonXmlProperty(isAttribute = true)
+    private String source;
+
+    @JacksonXmlProperty(isAttribute = true)
+    private String target;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
 }
