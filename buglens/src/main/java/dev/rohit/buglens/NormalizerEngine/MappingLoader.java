6d96d4916435e947f06b0610c8fbe458e706d046
rohitgh2024ju <rohitstudying2004@gmail.com>
2026-08-24 22:09:55 +0530

    engine directory setup

---
+++ buglens/src/main/java/dev/rohit/buglens/NormalizerEngine/MappingLoader.java
@@ -0,0 +1,5 @@
+package dev.rohit.buglens.NormalizerEngine;
+
+public class MappingLoader {
+
+}
---
+++ buglens/src/main/java/dev/rohit/buglens/NormalizerEngine/model/NormalizedEvent.java
@@ -0,0 +1,5 @@
+package dev.rohit.buglens.NormalizerEngine.model;
+
+public class NormalizedEvent {
+
+}
