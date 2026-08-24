8dc00d789e247ca0ff35a34ac9d842578849c64f
rohitgh2024ju <rohitstudying2004@gmail.com>
2026-08-24 21:50:08 +0530

    implement spring boot log parser

--- buglens/src/main/java/dev/rohit/buglens/IngestionEngine/IngestionEngine.java
+++ buglens/src/main/java/dev/rohit/buglens/IngestionEngine/IngestionEngine.java
@@ -3,7 +3,6 @@ package dev.rohit.buglens.IngestionEngine;
 import dev.rohit.buglens.IngestionEngine.format.FormatDetector;
 
 public class IngestionEngine {
-
     public static void main(String[] args) {
         FormatDetector formatDetector = new FormatDetector();
         String parser = formatDetector.detect();
--- buglens/src/main/java/dev/rohit/buglens/ParserEngine/ParserEngine.java
+++ buglens/src/main/java/dev/rohit/buglens/ParserEngine/ParserEngine.java
@@ -2,17 +2,50 @@ package dev.rohit.buglens.ParserEngine;
 
 import java.io.IOException;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import dev.rohit.buglens.IngestionEngine.Reader.LogReader;
+import dev.rohit.buglens.IngestionEngine.format.FormatDetector;
 
 // coordinates everything
 public class ParserEngine {
-    String inputPath;
+    private final String inputPath;
+    private final LogParser logParser;
 
-    ParserEngine(String inputPath) throws JSONException, IOException {
+    public ParserEngine(String inputPath, String parserClass) {
+        this.inputPath = inputPath;
+        this.logParser = new ParserRegistry(parserClass).find();
+    }
+
+    public void runParser() throws JSONException, IOException {
         LogReader logReader = new LogReader(inputPath);
         JSONObject logData = logReader.readFile(-1);
+        JSONArray logArray = logData.getJSONArray("logs");
+
+        for (Object logObj : logArray) {
+            String logStr = logObj.toString();
+            JSONObject parsedData = logParser.parse(logStr);
+            System.out.println(parsedData.toString(2));
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            FormatDetector formatDetector = new FormatDetector();
+            String parser = formatDetector.detect();
+            System.out.println(parser);
+
+            ParserEngine parserEngine = new ParserEngine(
+                    "buglens/logs/output.jsonl",
+                    parser);
+
+            parserEngine.runParser();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
